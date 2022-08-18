03ebda9647bd8dfb834063fbe0d5eec85f642fc5
Zach Hannum <zacharyhannum@gmail.com>
2022-08-18 00:03:00 -0400

    Basic sntp time example

--- main/main.cpp
+++ main/main.cpp
@@ -8,105 +8,162 @@
 */
 
 #include <iostream>
-#include <thread>
-#include <chrono>
-#include <memory>
-#include <string>
-#include <sstream>
-#include <esp_pthread.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <esp_log.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_sntp.h"
 
-using namespace std::chrono;
+static const char *TAG = "example";
+static void obtain_time(void);
+static void initialize_sntp(void);
 
-const auto sleep_time = seconds
+void time_sync_notification_cb(struct timeval *tv)
 {
-    5
-};
-
-void print_thread_info(const char *extra = nullptr)
-{
-    std::stringstream ss;
-    if (extra) {
-        ss << extra;
-    }
-    ss << "Core id: " << xPortGetCoreID()
-       << ", prio: " << uxTaskPriorityGet(nullptr)
-       << ", minimum free stack: " << uxTaskGetStackHighWaterMark(nullptr) << " bytes.";
-    ESP_LOGI(pcTaskGetTaskName(nullptr), "%s", ss.str().c_str());
+    ESP_LOGI(TAG, "Notification of a time synchronization event");
 }
 
-void thread_func_inherited()
-{
-    while (true) {
-        print_thread_info("This is the INHERITING thread with the same parameters as our parent, including name. ");
-        std::this_thread::sleep_for(sleep_time);
-    }
-}
-
-void spawn_another_thread()
-{
-    // Create a new thread, it will inherit our configuration
-    std::thread inherits(thread_func_inherited);
-
-    while (true) {
-        print_thread_info();
-        std::this_thread::sleep_for(sleep_time);
-    }
-}
-
-void thread_func_any_core()
-{
-    while (true) {
-        print_thread_info("This thread (with the default name) may run on any core.");
-        std::this_thread::sleep_for(sleep_time);
-    }
-}
-
-void thread_func()
-{
-    while (true) {
-        print_thread_info();
-        std::this_thread::sleep_for(sleep_time);
-    }
-}
-
-esp_pthread_cfg_t create_config(const char *name, int core_id, int stack, int prio)
-{
-    auto cfg = esp_pthread_get_default_config();
-    cfg.thread_name = name;
-    cfg.pin_to_core = core_id;
-    cfg.stack_size = stack;
-    cfg.prio = prio;
-    return cfg;
-}
+#ifndef INET6_ADDRSTRLEN
+#define INET6_ADDRSTRLEN 48
+#endif
 
 extern "C" void app_main(void)
 {
-    // Create a thread using deafult values that can run on any core
-    auto cfg = esp_pthread_get_default_config();
-    esp_pthread_set_cfg(&cfg);
-    std::thread any_core(thread_func_any_core);
 
-    // Create a thread on core 0 that spawns another thread, they will both have the same name etc.
-    cfg = create_config("Thread 1", 0, 3 * 1024, 5);
-    cfg.inherit_cfg = true;
-    esp_pthread_set_cfg(&cfg);
-    std::thread thread_1(spawn_another_thread);
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    // Is time set? If not, tm_year will be (1970 - 1900).
+    if (timeinfo.tm_year < (2016 - 1900))
+    {
+        ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
+        obtain_time();
+        // update 'now' variable with current time
+        time(&now);
+    }
 
-    // Create a thread on core 1.
-    cfg = create_config("Thread 2", 1, 3 * 1024, 5);
-    esp_pthread_set_cfg(&cfg);
-    std::thread thread_2(thread_func);
+    char strftime_buf[64];
+
+    // Set timezone to Eastern Standard Time and print local time
+    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    tzset();
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
 
     // Let the main task do something too
-    while (true) {
-        std::stringstream ss;
-        ss << "core id: " << xPortGetCoreID()
-           << ", prio: " << uxTaskPriorityGet(nullptr)
-           << ", minimum free stack: " << uxTaskGetStackHighWaterMark(nullptr) << " bytes.";
-        ESP_LOGI(pcTaskGetTaskName(nullptr), "%s", ss.str().c_str());
-        std::this_thread::sleep_for(sleep_time);
+    while (true)
+    {
+        time(&now);
+        localtime_r(&now, &timeinfo);
+        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+        ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void obtain_time(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /**
+     * NTP server address could be aquired via DHCP,
+     * see following menuconfig options:
+     * 'LWIP_DHCP_GET_NTP_SRV' - enable STNP over DHCP
+     * 'LWIP_SNTP_DEBUG' - enable debugging messages
+     *
+     * NOTE: This call should be made BEFORE esp aquires IP address from DHCP,
+     * otherwise NTP option would be rejected by default.
+     */
+#ifdef LWIP_DHCP_GET_NTP_SRV
+    sntp_servermode_dhcp(1); // accept NTP offers from DHCP server, if any
+#endif
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    initialize_sntp();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = {0};
+    int retry = 0;
+    const int retry_count = 15;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count)
+    {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+    ESP_ERROR_CHECK(example_disconnect());
+}
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+
+/*
+ * If 'NTP over DHCP' is enabled, we set dynamic pool address
+ * as a 'secondary' server. It will act as a fallback server in case that address
+ * provided via NTP over DHCP is not accessible
+ */
+#if LWIP_DHCP_GET_NTP_SRV && SNTP_MAX_SERVERS > 1
+    sntp_setservername(1, "pool.ntp.org");
+
+#if LWIP_IPV6 && SNTP_MAX_SERVERS > 2 // statically assigned IPv6 address is also possible
+    ip_addr_t ip6;
+    if (ipaddr_aton("2a01:3f7::1", &ip6))
+    { // ipv6 ntp source "ntp.netnod.se"
+        sntp_setserver(2, &ip6);
+    }
+#endif /* LWIP_IPV6 */
+
+#else /* LWIP_DHCP_GET_NTP_SRV && (SNTP_MAX_SERVERS > 1) */
+    // otherwise, use DNS address from a pool
+    sntp_setservername(0, "time.windows.com");
+
+    sntp_setservername(1, "pool.ntp.org"); // set the secondary NTP server (will be used only if SNTP_MAX_SERVERS > 1)
+#endif
+
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+#endif
+    sntp_init();
+
+    ESP_LOGI(TAG, "List of configured NTP servers:");
+
+    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i)
+    {
+        if (sntp_getservername(i))
+        {
+            ESP_LOGI(TAG, "server %d: %s", i, sntp_getservername(i));
+        }
+        else
+        {
+            // we have either IPv4 or IPv6 address, let's print it
+            char buff[INET6_ADDRSTRLEN];
+            ip_addr_t const *ip = sntp_getserver(i);
+            if (ipaddr_ntoa_r(ip, buff, INET6_ADDRSTRLEN) != NULL)
+                ESP_LOGI(TAG, "server %d: %s", i, buff);
+        }
     }
 }
